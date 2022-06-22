e057835a2fa4dab187eb52c47b5c18b155f81b87
Alessandro Genovese <alegeno88@gmail.com>
2022-06-22 16:46:41 +0000

    esercizio 06 completato

--- ToLearn/Impara_lhaskell/ep06/Esercizi.hs
+++ ToLearn/Impara_lhaskell/ep06/Esercizi.hs
@@ -17,25 +17,49 @@ init' [] = error "Non ci sono elementi in questa lista"
 init' [x] = []
 init' (x:xs) = x:init' xs 
 
-take' = undefined
-drop' = undefined
-null' = undefined
-
-
 reverse' [] = []
 reverse' (x:xs) = reverse' xs ++ [x]
 
+take' 0 _ = []
+take' 1 xs = head' xs
+take' n (x:xs) = x ++ take' (n-1) xs 
 
-elem' = undefined
-replicate' = undefined
-maximum' = undefined
-mininum' = undefined
-sum' = undefined
-product' = undefined
+drop' 0 xs = xs
+drop' _ [] = []
+drop' n (x:xs) = drop' (n-1) xs
+
+--null' xs = length' xs == 0 
+null' [] = True
+null' _ = False
+
+elem' _ [] = False
+elem' y (x:xs) = (y == x) || elem' y xs 
+
+replicate' n x = [x | _ <- [1..n]]
+
+maximum' [] = error "Non ci sono elementi in questa lista"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+mininum' [] = error "Non ci sono elementi in questa lista"
+minimum' [x] = x
+minimum' (x:xs) = min x (minimum' xs)
+
+sum' [] = error "Non ci sono elementi in questa lista"
+sum' [x] = x
+sum' (x:xs) = x + sum' xs
+
+product' [] = error "Non ci sono elementi in questa lista"
+product' [x] = x
+product' (x:xs) = x * product' xs
 
 -- tuple
 fst' (a,_) = a
-snd' = undefined
+snd' (_, a)= a
 
-zip' = undefined
-unzip' = undefined
+zip' [] _ = []
+zip' _ [] = []
+zip' (x:xs) (y:ys) = (x,y):(zip' xs ys)
+
+unzip' [] = ([],[])
+unzip' ((a,b):xs) = (a:fst'(unzip' xs), b:snd'(unzip' xs))
